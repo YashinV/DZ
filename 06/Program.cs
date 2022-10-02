b01e1daa1d4055e260cd57db891e5ec607c06d5f
123 <vasya_yashin@list.ru>
2022-10-02 21:59:01 +0300

    tr

---
+++ 06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 ==0 ){
+ Console.WriteLine(" число четное ");
+}
+else {
+    Console.WriteLine(" число  не четное ");
+}
+
+Console.WriteLine();
