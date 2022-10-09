de14378b0dc49713336f48157d2c2270eb12b8d0
vasili <114146396+YashinV@users.noreply.github.com>
2022-10-09 15:41:55 +0300

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите от 100 до 999 число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>=100 && a<=999)
+{
+int DesreaseByTen = a / 10;
+int LastDigit = DesreaseByTen  %10;
+Console.WriteLine(" Вторая цифра числа = " +LastDigit );
+}
+else
+{
+Console.WriteLine("вы ввели не правильное число, повторите попытку 100 -999 : ");
+a=Convert.ToInt32(Console.ReadLine());
+}
+{
+int DesreaseByTen = a / 10;
+int LastDigit = DesreaseByTen  %10;
+Console.WriteLine(" Вторая цифра числа = " +LastDigit );
+}
---
+++ 13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+string a_text = Convert.ToString(a);
+if(a_text.Length > 2)
+{
+    Console.WriteLine("Третья цифра : " + a_text[0]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+
+}
+//     (a>=99);
+// Console.Write("Третьей цифры нет, число двухзначное"+Convert.ToSring(a%100));
+//     (a<=100);
+//     Console.Write("Третья цифра : "+Convert.ToSring(a%100));
